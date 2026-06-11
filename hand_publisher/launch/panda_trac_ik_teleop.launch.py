78543263cc248c49aa79cf94638acc64663d817d
Hartvi <j.hartvich@gmail.com>
2026-06-11 21:49:58 +0200

    modify trac ik node to get robot description from robot state publisher parameter

--- hand_publisher/launch/panda_trac_ik_teleop.launch.py
+++ hand_publisher/launch/panda_trac_ik_teleop.launch.py
@@ -1,8 +1,5 @@
 from pathlib import Path
-import os
-import xml.etree.ElementTree as ET
 
-import xacro
 import yaml
 
 from ament_index_python.packages import get_package_share_directory
@@ -24,43 +21,6 @@ def as_bool(value: object) -> bool:
     return str(value).lower() in ("true", "1", "yes", "on")
 
 
-def resolve_package_path(path_str: str) -> str:
-    path_str = os.path.expandvars(os.path.expanduser(path_str))
-    if path_str.startswith("package://"):
-        without_scheme = path_str.removeprefix("package://")
-        package_name, relative_path = without_scheme.split("/", 1)
-        return str(Path(get_package_share_directory(package_name)) / relative_path)
-    return path_str
-
-
-def resolve_robot_file(package_name: str, relative_path: str) -> str:
-    return str(Path(get_package_share_directory(package_name)) / relative_path)
-
-
-def weld_urdf_to_world(urdf_xml: str, world_link: str, base_link: str) -> str:
-    root = ET.fromstring(urdf_xml)
-
-    existing_links = {
-        elem.attrib["name"] for elem in root.findall("link") if "name" in elem.attrib
-    }
-    existing_joints = {
-        elem.attrib["name"] for elem in root.findall("joint") if "name" in elem.attrib
-    }
-
-    if world_link not in existing_links:
-        root.insert(0, ET.Element("link", name=world_link))
-
-    joint_name = f"{world_link}_to_{base_link}_fixed"
-    if joint_name not in existing_joints:
-        fixed_joint = ET.Element("joint", name=joint_name, type="fixed")
-        ET.SubElement(fixed_joint, "parent", link=world_link)
-        ET.SubElement(fixed_joint, "child", link=base_link)
-        ET.SubElement(fixed_joint, "origin", xyz="0 0 0", rpy="0 0 0")
-        root.append(fixed_joint)
-
-    return ET.tostring(root, encoding="unicode")
-
-
 def launch_setup(context: LaunchContext, *args, **kwargs):
     robot_config_path = LaunchConfiguration("robot_config").perform(context)
     controller_delay = LaunchConfiguration("controller_delay")
@@ -69,30 +29,9 @@ def launch_setup(context: LaunchContext, *args, **kwargs):
         config = yaml.safe_load(f)
 
     robot_cfg = config["robot"]
-    description_cfg = robot_cfg["description"]
-    xacro_path = resolve_robot_file(
-        description_cfg["package"], description_cfg["xacro"]
-    )
-
-    raw_xacro_args = robot_cfg.get("xacro_args", {})
-    xacro_args = {
-        key: str(value).lower() if isinstance(value, bool) else str(value)
-        for key, value in raw_xacro_args.items()
-    }
-    for key, value in list(xacro_args.items()):
-        if value.startswith("package://"):
-            xacro_args[key] = resolve_package_path(value)
-
-    doc = xacro.process_file(xacro_path, mappings=xacro_args)
-    urdf_xml = doc.toxml()
-
     links_cfg = robot_cfg.get("links", {})
     base_link = links_cfg.get("base", "base_link")
     tip_link = links_cfg.get("tip", "end_effector_link")
-    world_link = links_cfg.get("world", "world")
-
-    if as_bool(robot_cfg.get("weld_to_world", False)):
-        urdf_xml = weld_urdf_to_world(urdf_xml, world_link, base_link)
 
     use_sim_time = as_bool(
         robot_cfg.get("robot_state_publisher", {}).get("use_sim_time", True)
@@ -112,9 +51,9 @@ def launch_setup(context: LaunchContext, *args, **kwargs):
                             "base_link": base_link,
                             "tip_link": tip_link,
                             "ik_service": "/solve_ik",
+                            "robot_description_node": "/robot_state_publisher",
                             "timeout": 0.02,
                             "eps": 1e-5,
-                            "robot_description": urdf_xml,
                             "use_sim_time": use_sim_time,
                         }
                     ],
